662e152a0c9e578b118d42645f3b48d75036748d
Michael Duren <michaeld@michaelduren.com>
2023-06-02 11:28:43 -0500

    added useEffect and fetch request for items in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-
-import Header from '../Header/Header.jsx'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from '../Header/Header.jsx';
 import './App.css';
 
-
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
+  const [listItems, setListItems] = useState([]);
+
+  const getAllItems = () => {
+    return axios
+      .get('/items')
+      .then((response) => response.data)
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getAllItems().then(setListItems);
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <div>
+          <h2>Grocery List</h2>
+          <ul>
+            {listItems &&
+              listItems.map((item) => {
+                return (
+                  <li key={item.id}>
+                    {item.name}, quanity: {item.quantity} {item.unit}
+                  </li>
+                );
+              })}
+          </ul>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
 
 export default App;
