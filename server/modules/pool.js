5eb90387802ff160bd0a20048c792c8fce20218f
Andreas Copan <avcopan@gmail.com>
2023-06-16 11:14:12 -0400

    Configures pool for class-platform compatibility

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,20 @@
-const pg = require('pg');
+const pg = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const pool = new pg.Pool({
-  host: 'localhost',
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+pool_params = {
+  host: "localhost",
   port: 5432,
-  database: 'shoppinglist',
-});
+  database: "shoppinglist",
+};
+
+if (DB_PASSWORD) {
+  pool_params.user = "postgres";
+  pool_params.password = DB_PASSWORD;
+}
+
+const pool = new pg.Pool(pool_params);
 
 module.exports = pool;
