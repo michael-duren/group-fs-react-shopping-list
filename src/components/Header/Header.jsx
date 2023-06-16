49ee703e1028f42028fa38228295f3fc0c177276
Michael Duren <michaeld@michaelduren.com>
2023-06-16 11:25:41 -0500

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
 import { AppBar } from '@mui/material';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 
 function Header() {
   return (
     <AppBar position="static">
       <header className="banner-header">
         <h1>My Shopping List</h1>
+        <LocalGroceryStoreIcon fontSize="large" />
       </header>
     </AppBar>
   );
